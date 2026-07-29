2d1ed06357af40a7412eb60828014293181afa08
[Arjav Jain] <[arjav9191@gmail.com]>
2026-07-29 14:46:15 +0530

    docs: improve readability of ScarySetter example

--- koans/about_attribute_access.py
+++ koans/about_attribute_access.py
@@ -178,17 +178,20 @@ class AboutAttributeAccess(Koan):
     # ------------------------------------------------------------------
 
     class ScarySetter:
-        def __init__(self):
-            self.num_of_coconuts = 9
-            self._num_of_private_coconuts = 2
+    """Demonstrates how __setattr__ intercepts attribute assignments."""
 
-        def __setattr__(self, attr_name, value):
-            new_attr_name =  attr_name
+    def __init__(self):
+        self.num_of_coconuts = 9
+        self._num_of_private_coconuts = 2
 
-            if attr_name[0] != '_':
-                new_attr_name = "altered_" + new_attr_name
+    def __setattr__(self, attr_name, value):
+        """Prefix public attributes with 'altered_' before storing them."""
+        new_attr_name = attr_name
 
-            object.__setattr__(self, new_attr_name, value)
+        if not attr_name.startswith('_'):
+            new_attr_name = f"altered_{attr_name}"
+
+        object.__setattr__(self, new_attr_name, value)
 
     def test_it_modifies_external_attribute_as_expected(self):
         setter = self.ScarySetter()
